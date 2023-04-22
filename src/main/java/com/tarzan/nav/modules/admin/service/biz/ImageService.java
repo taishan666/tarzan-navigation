c2f65bef920a8319a93ff3d6b66f5a3afbac28a4
tarzan <1334512682@qq.com>
2023-04-22 20:19:30 +0800

    代码优化

--- src/main/java/com/tarzan/nav/modules/admin/service/biz/ImageService.java
+++ src/main/java/com/tarzan/nav/modules/admin/service/biz/ImageService.java
@@ -6,6 +6,7 @@ import com.tarzan.nav.modules.admin.mapper.biz.ImageMapper;
 import com.tarzan.nav.modules.admin.model.biz.BizImage;
 import com.tarzan.nav.utils.StringUtil;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
@@ -17,21 +18,23 @@ import java.util.Objects;
 @Service
 public class ImageService extends ServiceImpl<ImageMapper, BizImage> {
 
+    @Value("${server.port}")
+    private Integer port;
 
     public BizImage upload(String src){
-        if(StringUtil.isNotBlank(src)){
-            byte[] bytes=HttpUtil.downloadBytes(src.split(",")[0]);
-            String md5 = DigestUtils.md5Hex(bytes);
-            BizImage oldImage=baseMapper.selectById(md5);
-            if(Objects.isNull(oldImage)){
-                String base64="data:"+src.split(",")[1]+";base64,"+ Base64.getEncoder().encodeToString(bytes);
-                BizImage newImage=BizImage.builder().id(md5).base64(base64).build();
-                baseMapper.insert(newImage);
-                return newImage;
-            }else {
-                return oldImage;
-            }
+        if(StringUtil.isBlank(src)){
+            src="http://localhost:"+port+"/favicon.ico,image/ico";
         }
-        return null;
+        byte[] bytes=HttpUtil.downloadBytes(src.split(",")[0]);
+        String md5 = DigestUtils.md5Hex(bytes);
+        BizImage oldImage=baseMapper.selectById(md5);
+        if(Objects.isNull(oldImage)){
+            String base64="data:"+src.split(",")[1]+";base64,"+ Base64.getEncoder().encodeToString(bytes);
+            BizImage newImage=BizImage.builder().id(md5).base64(base64).build();
+            baseMapper.insert(newImage);
+            return newImage;
+        }else {
+            return oldImage;
+        }
     }
 }
