47d8d08587c9b08465a3dc62c09b2165f054bc7a
tarzan <liuyapeng@bdtd.com>
2023-05-26 19:14:20 +0800

    代码优化

--- src/main/java/com/tarzan/nav/modules/front/NavApiController.java
+++ src/main/java/com/tarzan/nav/modules/front/NavApiController.java
@@ -155,4 +155,9 @@ public class NavApiController {
         }
     }
 
+    @GetMapping("/hotSpot/all")
+    @ResponseBody
+    public ResponseVo hotSpotAll() {
+        return ResultUtil.vo(hotNewsService.hotSpot());
+    }
 }
