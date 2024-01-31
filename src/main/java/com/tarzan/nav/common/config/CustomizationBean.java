974b1a77ef4df80181dbab3022df8ae427dd177f
tarzan <1334512682@qq.com>
2024-01-31 15:24:43 +0800

    readme

--- src/main/java/com/tarzan/nav/common/config/CustomizationBean.java
+++ src/main/java/com/tarzan/nav/common/config/CustomizationBean.java
@@ -2,10 +2,8 @@ package com.tarzan.nav.common.config;
 
 import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,18 +19,6 @@ public class CustomizationBean implements WebServerFactoryCustomizer<UndertowSer
             webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 1024));
             deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
         });
-    }
-
-    @Bean
-    public static DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
-        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
-        /**
-         * setUsePrefix(false)用于解决一个奇怪的bug。在引入spring aop的情况下。
-         * 在@Controller注解的类的方法中加入@RequiresRole等shiro注解，会导致该方法无法映射请求，导致返回404。
-         * 加入这项配置能解决这个bug
-         */
-        defaultAdvisorAutoProxyCreator.setUsePrefix(true);
-        return defaultAdvisorAutoProxyCreator;
     }
 
 }
