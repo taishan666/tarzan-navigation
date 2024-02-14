a23847e570d08ef857c1e3b0aaa68ab1db67657d
tarzan <1334512682@qq.com>
2024-02-14 18:23:11 +0800

    代码优化

--- src/main/java/com/tarzan/nav/modules/aichat/service/impl/linkai/Gpt4AiServiceImpl.java
+++ src/main/java/com/tarzan/nav/modules/aichat/service/impl/linkai/Gpt4AiServiceImpl.java
@@ -2,6 +2,7 @@ package com.tarzan.nav.modules.aichat.service.impl.linkai;
 
 import com.tarzan.nav.modules.aichat.enums.AISourceEnum;
 import com.tarzan.nav.modules.aichat.vo.ChatRecordsVo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -11,6 +12,9 @@ import reactor.core.publisher.Flux;
 @Service
 public class Gpt4AiServiceImpl extends AbsLinkAiService {
 
+    @Value("${linkai.gpt4.day-limit}")
+    private Integer dayLimit;
+
     @Override
     public AISourceEnum source() {
         return AISourceEnum.CHAT_GPT_4;
@@ -19,6 +23,11 @@ public class Gpt4AiServiceImpl extends AbsLinkAiService {
     @Override
     public Flux<ChatRecordsVo> doAsyncAnswer(ChatRecordsVo response) {
         return super.doAsyncAnswer(response,source().getName());
+    }
+
+    @Override
+    protected int getMaxQaCnt(Integer userId) {
+        return dayLimit==null?super.getMaxQaCnt(userId):dayLimit;
     }
 
 }
--- src/main/java/com/tarzan/nav/modules/aichat/service/impl/linkai/WenXin4AiServiceImpl.java
+++ src/main/java/com/tarzan/nav/modules/aichat/service/impl/linkai/WenXin4AiServiceImpl.java
@@ -2,6 +2,7 @@ package com.tarzan.nav.modules.aichat.service.impl.linkai;
 
 import com.tarzan.nav.modules.aichat.enums.AISourceEnum;
 import com.tarzan.nav.modules.aichat.vo.ChatRecordsVo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -10,6 +11,10 @@ import reactor.core.publisher.Flux;
  */
 @Service
 public class WenXin4AiServiceImpl extends AbsLinkAiService {
+
+    @Value("${linkai.wenxin4.day-limit}")
+    private Integer dayLimit;
+
     @Override
     public Flux<ChatRecordsVo> doAsyncAnswer(ChatRecordsVo response) {
         return super.doAsyncAnswer(response,source().getName());
@@ -19,4 +24,10 @@ public class WenXin4AiServiceImpl extends AbsLinkAiService {
     public AISourceEnum source() {
         return AISourceEnum.WEI_XIN_4;
     }
+
+    @Override
+    protected int getMaxQaCnt(Integer userId) {
+        return dayLimit==null?super.getMaxQaCnt(userId):dayLimit;
+    }
+
 }
